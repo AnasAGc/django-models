8037e63d22e6dd90ebb0a5e6f08b4657ea9719b0
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-08-30 14:43:31 +0300

    fix tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -4,12 +4,12 @@ from django.urls import reverse
 # Create your tests here.
 class thingsTests(TestCase):
     def test_home_page_status_code(self):
-        url = reverse('snacks_list')
+        url = reverse('snack_list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_home_page_templete(self):
-        url = reverse('snacks_list')
+        url = reverse('snack_list')
         response = self.client.get(url)
-        self.assertTemplateUsed(response, 'snacks_list.html')
+        self.assertTemplateUsed(response, 'snack_list.html')
         self.assertTemplateUsed(response, 'base.html')
